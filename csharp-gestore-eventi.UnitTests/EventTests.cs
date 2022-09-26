affe2aa02b55866821fbefa60ff5d43bc8919f40
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-26 10:19:21 +0200

    add reserve seats

--- csharp-gestore-eventi.UnitTests/EventTests.cs
+++ csharp-gestore-eventi.UnitTests/EventTests.cs
@@ -1,5 +1,67 @@
+using FluentAssertions;
+
 namespace csharp_gestore_eventi.UnitTests;
 
 public class EventTests
 {
+    private Event _sut;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _sut = new Event("DummyTitle", DateTime.Now.AddDays(14), 30);
+    }
+    
+    [Test]
+    [TestCase(5, 5)]
+    [TestCase(15, 15)]
+    [TestCase(30, 30)]
+    public void ReserveSeats_ShouldAddReservedSeats_WhenThereIsEnoughSpace(int seatsToReserve, int expected)
+    {
+        _sut.ReserveSeats(seatsToReserve);
+        
+        _sut.ReservedSeats.Should().Be(expected);
+    }
+    
+    [Test]
+    [TestCase(31)]
+    [TestCase(35)]
+    [TestCase(40)]
+    public void ReserveSeats_ShouldThrowException_WhenThereIsNoSpace(int seatsToReserve)
+    {
+        Action action = () => _sut.ReserveSeats(seatsToReserve);
+
+        action
+            .Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("The maximum seats capacity is been reached.");
+    }
+    
+    [Test]
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(-12)]
+    public void ReserveSeats_ShouldThrowException_WhenNegativesOrZeroArePassed(int seatsToReserve)
+    {
+        Action action = () => _sut.ReserveSeats(seatsToReserve);
+
+        action
+            .Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("You must provide a positive number of seats.");
+    }
+    
+    [Test]
+    public void ReserveSeats_ShouldThrowException_WhenEventIsAlreadyOver()
+    {
+        var sut = new Event("DummyTitle", DateTime.Parse("2019/12/10 00:00:00"), 30);
+        var seatsToReserve = 5;
+        
+        Action action = () => sut.ReserveSeats(seatsToReserve);
+
+        action
+            .Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("You cannot reserve seats in a past event.");
+    }
 }
---
+++ csharp-gestore-eventi/Event.cs
@@ -0,0 +1,96 @@
+namespace csharp_gestore_eventi;
+
+public class Event
+{
+    private string _title;
+    public string Title
+    {
+        get => _title;
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value), "You must provide a title.");
+            }
+
+            _title = value;
+        }
+    }
+
+    private DateTime _date;
+    public DateTime Date
+    {
+        get => _date;
+
+        set
+        {
+            if (value < _date)
+            {
+                throw new InvalidOperationException("The date cannot be in the past.");
+            }
+
+            _date = value;
+        }
+    }
+
+    private int _maxSeats;
+    public int MaxSeats
+    {
+        get => _maxSeats;
+
+        private init
+        {
+            if (value <= 0)
+            {
+                throw new InvalidOperationException("You must provide at least one seat.");
+            }
+
+            _maxSeats = value;
+        }
+    }
+
+    private int _reservedSeats;
+    public int ReservedSeats
+    {
+        get => _reservedSeats;
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new InvalidOperationException("Negative numbers are not allowed.");
+            }
+
+            if (value > _maxSeats)
+            {
+                throw new InvalidOperationException("The maximum seats capacity is been reached.");
+            }
+
+            _reservedSeats = value;
+        }
+    }
+
+    public Event(string title, DateTime date, int maxSeats)
+    {
+        Title = title;
+        Date = date;
+        MaxSeats = maxSeats;
+        ReservedSeats = 0;
+    }
+
+    public void ReserveSeats(int seatsToReserve)
+    {
+        if (Date < DateTime.Now)
+        {
+            throw new InvalidOperationException("You cannot reserve seats in a past event.");
+        }
+        
+        if (seatsToReserve <= 0)
+        {
+            throw new InvalidOperationException("You must provide a positive number of seats.");
+        }
+        
+        ReservedSeats += seatsToReserve;
+    }
+}
